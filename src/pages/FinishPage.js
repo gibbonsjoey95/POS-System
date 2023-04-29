4d674d6f7ff30d9b27b33c254815ea3ccde3a3d3
Joey Gibbons <gibbonsjoey95@gmail.com>
2023-04-28 23:41:08 -0500

    edited finish page

--- src/pages/FinishPage.js
+++ src/pages/FinishPage.js
@@ -3,9 +3,6 @@ const FinishPage = () => {
     <div className='order-page'>
       <div className='page-title'>
         <h1>Finish</h1>
-        <button className='button blue' onClick={() => console.log('yes')}>
-          Test here
-        </button>
       </div>
     </div>
   );
