887dbd69bba7bd16416886873ba93196eea1f87d
Joey Gibbons <gibbonsjoey95@gmail.com>
2023-07-27 22:59:50 -0500

    got more work done on CC and Expiration input

--- src/components/PaymentsComponents/CreditCardInput.js
+++ src/components/PaymentsComponents/CreditCardInput.js
@@ -2,35 +2,6 @@ import { useState } from 'react';
 import NumericKeypad from '../NumericKeypad';
 
 const CreditCardInput = ({ creditCardInfo }) => {
-  // const [activeField, setActiveField] = useState('CreditCard');
-  // const [enteredNumbers, setEnteredNumbers] = useState('');
-
-  // const [expirationDate, setExpirationDate] = useState('');
-  // const [cvv, setCvv] = useState('');
-
-  // const handleCreditCardInputChange = (event) => {
-  //   const input = event.target.value;
-  //   const sanitizedInput = input.replace(/[^0-9]/g, ''); // Remove non-numeric characters
-  //   const formattedInput = sanitizedInput
-  //     .slice(0, 16) // Limit to a maximum of 16 digits
-  //     .replace(/(.{4})/g, '$1-') // Insert '-' after every 4 characters
-  //     .slice(0, -1); // Remove the trailing '-'
-
-  //   setEnteredNumbers(formattedInput);
-  // };
-
-  // const handleDeleteOneNumberFromCreditCard = () => {
-  //   if (enteredNumbers.length > 0) {
-  //     let updatedNumbers = enteredNumbers.slice(0, -1);
-
-  //     if (updatedNumbers.slice(-1) === '-') {
-  //       updatedNumbers = updatedNumbers.slice(0, -1);
-  //     }
-
-  //     setEnteredNumbers(updatedNumbers);
-  //   }
-  // };
-
   const [activeField, setActiveField] = useState('CreditCard');
   const [enteredNumbers, setEnteredNumbers] = useState('');
   const [expirationDate, setExpirationDate] = useState('');
@@ -38,18 +9,16 @@ const CreditCardInput = ({ creditCardInfo }) => {
 
   const formatCreditCardNumber = (input) => {
     const sanitizedInput = input.replace(/\D/g, '').slice(0, 16);
-    const formattedInput = sanitizedInput.replace(/(.{4})/g, '$1-');
+    const formattedInput = sanitizedInput.replace(/(.{4})(?=.)/g, '$1-');
 
     return formattedInput;
   };
 
   const formatExpirationDate = (input) => {
     const sanitizedInput = input.replace(/\D/g, '');
-    const month = sanitizedInput.slice(0, 2);
-    const year = sanitizedInput.slice(2, 4);
+    const formattedInput = sanitizedInput.replace(/(.{2})(?=.)/g, '$1/');
 
-    // Format the expiration date as "mm/yy"
-    return `${month}/${year}`;
+    return formattedInput;
   };
 
   const handleCreditCardKeyPress = (number) => {
@@ -64,51 +33,21 @@ const CreditCardInput = ({ creditCardInfo }) => {
   };
 
   const handleExpirationDateKeyPress = (number) => {
-    let sanitizedInput;
-    let month = '';
-    let year;
-
     if (expirationDate.length < 5) {
       setExpirationDate(expirationDate + number);
 
-      sanitizedInput = (expirationDate + number).replace(/\D/g, '');
+      const sanitizedInput = (expirationDate + number).replace(/\D/g, '');
+      const formattedInput = sanitizedInput.replace(/(.{2})(?=.)/g, '$1/');
 
-      if (sanitizedInput[0] != 1) {
-        sanitizedInput = `0${sanitizedInput}`;
-      }
-      month = sanitizedInput.slice(0, 2);
-      year = sanitizedInput.slice(2, 4);
-
-      setExpirationDate(`${month}/${year}`);
+      setExpirationDate(formattedInput);
     }
   };
 
   const handleCvvKeyPress = (number) => {
-    setCvv(cvv + number);
+    if (cvv.length < 3) {
+      setCvv(cvv + number);
+    }
   };
-
-  // Keep here just until done to be able to use for future reference
-  // const handleInputChange = (event) => {
-  //   const { name, value } = event.target;
-
-  //   // Update the corresponding state variable based on the field name
-  //   switch (name) {
-  //     case 'cardNumber':
-  //       console.log('here');
-  //       setEnteredNumbers(value);
-  //       break;
-  //     case 'expirationDate':
-  //       console.log('typed');
-  //       setExpirationDate(value);
-  //       break;
-  //     case 'cvv':
-  //       console.log('cvv');
-  //       setCvv(value);
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // };
 
   // handles keyboard entries
   const handleInputChange = (event) => {
@@ -224,6 +163,7 @@ const CreditCardInput = ({ creditCardInfo }) => {
         // onChange={handleInputChange}
         // onChange={handleKeyPress}
         onChange={handleInputChange}
+        // onKeyDown={handleInputChange}
         onFocus={handleCreditFocus}
         maxLength={19}
         minLength={19}
